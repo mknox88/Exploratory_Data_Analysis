8cce141363d210ccb01c9972d9bd615950818c70
Mark Knox <mknox88@gmail.com>
2014-12-06 14:28:18 -0700

    Converted scripts to use data.table and fread() instead of read.csv2.sql for better performance in reading data from source file.

--- plot2.R
+++ plot2.R
@@ -16,10 +16,12 @@ zipFile <- paste0(dataDir, "/exdata_data_household_power_consumption.zip")
 dataFile <- paste0(dataDir, "/household_power_consumption.txt")
 outputFile <- "plot2.png"
 
+## create data directory if it doesn't already exist
 if (!file.exists(dataDir)) {
     dir.create(dataDir)
 }
 
+## download and unzip data file if it doesn't already exist
 if (!file.exists(dataFile)) {
     if (!file.exists(zipFile)) {
         download.file(fileURL, zipFile, method="curl")
@@ -27,17 +29,41 @@ if (!file.exists(dataFile)) {
     unzip(zipFile, exdir=dataDir)
 }
 
-## 1. Load the dataset for the date range: 2/1/2007 - 2/2/2007
-library(sqldf)
-data <- read.csv2.sql(dataFile, sql="select * from file where Date in ('1/2/2007', '2/2/2007')"
-                      , header=TRUE, sep=";")
+## 2. Load the dataset for the date range: 2/1/2007 - 2/2/2007
+
+## I used read.csv2.sql at first because it was very simple and flexible, but it is much slower than
+## using fread and it doesn't handle NA string replacement (e.g. na.strings argument), which could
+## be a significant drawback depending on the dataset. As a result, I have rewritten this script using
+## fread().
+
+library(data.table)
+
+## pass shell command to fread() to select only rows that begin with '1/2/2007' (d/m/Y) or '2/2/2007';
+## read matching rows from shell command into a data.table, while replacing '?' in data with NA;
+## make all columns of type character temporarily to handle potential '?' in data;
+## we will change column types to appropriate types later.
+strInput <- paste("grep '^[12]/2/2007'", dataFile)
+data <- fread(strInput, header=FALSE, sep=";", na.strings="?", colClasses="character")
+
+## get column names by reading in header row only from data file
+## set column names of data.table to what we read from header row
+colNames <- names(fread(dataFile, header=TRUE, sep=";", nrows=0))
+setnames(data, colNames)
 
 
-## 2. Convert Date and Time variables to Date/Time classes using strptime() and as.Date() functions
-data$Time <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
-data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+## 3. Convert Date and Time variables to Date/Time classes using as.Date() and strptime() functions
+##    Convert numeric variables to numeric classes
 
-## 3. Construct plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels
+## need to concatenate Date & Time variables before calling strptime() or the Date part will be today's date
+## need to coerce time as POSIXct type because data.table will not support POSIXlt types
+## convert columns 3-9 to be numeric type - ?s have already been converted to NA
+data[, Time := as.POSIXct(strptime(paste(Date, Time), format="%d/%m/%Y %H:%M:%S"))]
+data[, Date := as.Date(Date, format="%d/%m/%Y")]
+data[, 3:9 := lapply(.SD, as.numeric), .SDcols=3:9, with=FALSE]
+
+
+## 4. Construct plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels
+
 png(outputFile, width=480, height=480)  ## open the PNG device for plotting
 
 ## plot Global_active_power variable versus Time as a line plot
